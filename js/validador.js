0f2cdec3a57a21b66af1c0327879526bdc5cd373
aleInte15 <jaaleinte15@gmail.com>
2025-02-28 19:58:57 -0600

    se agrega maquetado con su funcionalidad para validar formulario

---
+++ js/validador.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('loginForm');
+    form.addEvetListener('submit', function (event) {
+        event.preventDefault();
+        const username = document.getElementById('username').value.trim();
+        const password = document.getElementById('password').value.trim();
+        if (username === "" || password === "") {
+            alert("No se permiten espacios en blanco.");
+        } else {
+            alert("Login exitoso.");
+            form.submit();
+        }
+    });
+});
